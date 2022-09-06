f9885da84b00b77954d99bce75a93c70cea5c13f
GFilippos <gomelisfil@gmail.com>
2022-09-06 21:25:18 +0300

    Service layer completed

---
+++ src/main/java/com/sha/springbootproductseller/service/PurchaseServiceImp.java
@@ -0,0 +1,32 @@
+package com.sha.springbootproductseller.service;
+
+import com.sha.springbootproductseller.model.Purchase;
+import com.sha.springbootproductseller.repository.PurchaseRepository;
+import com.sha.springbootproductseller.repository.projection.PurchaseItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class PurchaseServiceImp implements PurchaseService{
+
+    private final PurchaseRepository purchaseRepository;
+
+    public PurchaseServiceImp(PurchaseRepository purchaseRepository) {
+        this.purchaseRepository = purchaseRepository;
+    }
+
+    @Override
+    public Purchase savePurchase(Purchase purchase) {
+        purchase.setPurchaseTime(LocalDateTime.now());
+
+        return purchaseRepository.save(purchase);
+    }
+
+    @Override
+    public List<PurchaseItem> findPurchaseItemOfUser(Long userId) {
+        return purchaseRepository.findAllPurchasesOfUser(userId);
+    }
+}
